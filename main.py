b8f51509e670f4ab653ced0ffa59a17e3ec03c7b
Tomáš Honsa <Tomas.Honsa@student.souepl.cz>
2024-10-16 09:24:13 +0200

    m

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import machine
+import time
 
-led = machine.Pin(1, machine.Pin.OUT)
-
-led.value(1)
+led1 = machine.Pin(0, machine.Pin.OUT)
+led2 = machine.Pin(1, machine.Pin.OUT)
+led3 = machine.Pin(2, machine.Pin.OUT)
+led4 = machine.Pin(3, machine.Pin.OUT)
+while True:
+    time.sleep(0.3)
+    led1.value(1)
+    time.sleep(0.3)
+    led2.value(1)
+    time.sleep(0.3)
+    led3.value(1)
+    time.sleep(0.3)
+    led4.value(1)  
+    time.sleep(1.5)
+    led1.value(0)
+    led2.value(0)
+    led3.value(0)
+    led4.value(0)
